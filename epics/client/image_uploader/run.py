819b329a0e6aad5972e46a3f56e6fa063e7e4fd4
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-01-26 14:30:55 -0600

    image_uploader client: write image_analysis_finished to lastAnalyzed PVs
    After the image_uploader subscribes to image PVs and attached callbacks,
    have it subscribe to the image_analysis_finished_ch channel in Redis
    (temporarily! this will be replaced by a dedicated pubsub) and set
    LastAnalyzedShotID PVs
    
    Adds redis and python-dotenv to requirements.txt

--- epics/client/image_uploader/run.py
+++ epics/client/image_uploader/run.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+import os
+
 import requests
+from utils.redis import get_redis_client
+
+from utils.types import ImageAnalysisFinishedMessage
 
 IMAGE_PVS = [
     "device1/image",
@@ -11,26 +16,48 @@ IMAGE_PVS = [
 WORK_QUEUE_NUM_WORKERS = 12
 IMAGE_BACKEND_ENDPOINT_URL = "http://localhost:5000"
 
+# the PV whose value to set to shot ID given an image_analysis_finished message 
+# device_name field
+last_analyzed_pv_from_device_name = {
+    'Spectrometer_LowEnergy': "Electrons:Spectrometer_SpectrumPNG:LastAnalyzedShotID",
+    'eScreenA': "Electrons:Spectrometer_SpectrumPNG:LastAnalyzedShotID",
+}
+
 from p4p.client.thread import Context as P4PContext
 from p4p.rpc import WorkQueue
 
 if TYPE_CHECKING:
     from p4p import Value as P4PValue
 
+work_queue = WorkQueue(WORK_QUEUE_NUM_WORKERS)
+p4p_context = P4PContext('pva', queue=work_queue)
+
 def send_to_image_backend(value: P4PValue):
     requests.post(IMAGE_BACKEND_ENDPOINT_URL, data={'device_name': value.device_name, 'shot_id': value.shot_id},
                   files={'image_data': value.image_data},
                  )
 
-def main():
-    # Subscribe to the image PVs
-    work_queue = WorkQueue(WORK_QUEUE_NUM_WORKERS)
-    p4p_context = P4PContext('pva', queue=work_queue)
-
+def subscribe_to_PVs_for_upload():
     for image_pv in IMAGE_PVS:
         p4p_context.monitor(image_pv, send_to_image_backend)
 
+def listen_for_and_process_analysis_complete_messages():
+    redis_client = get_redis_client()
+    ps = redis_client.pubsub()
+    ps.subscribe('image_analysis_finished_ch')
+
+    while True:
+        message: ImageAnalysisFinishedMessage = ps.get_message()
+
+        try:
+            pv_name = last_analyzed_pv_from_device_name[message['device_name']]
+        except KeyError:
+            continue
+
+        p4p_context.set(pv_name, message['shot_id'])
+
 
 if __name__ == '__main__':
-    main()
+    subscribe_to_PVs_for_upload()
+    listen_for_and_process_analysis_complete_messages()
 
