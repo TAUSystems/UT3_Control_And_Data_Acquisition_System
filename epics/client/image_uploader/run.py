0fb93e94000a1775736bbc1fca04c8cc6545db66
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-02-23 13:30:39 -0800

    Minor correction

--- epics/client/image_uploader/run.py
+++ epics/client/image_uploader/run.py
@@ -69,7 +69,7 @@ def send_to_image_backend(device_name: str, image_data: NTNDArray):
 
     response_data = response.json()
 
-    if ('message' not in response_data) or (not response_data['message'].startswith("received image data")):
+    if ('message' not in response_data) or (not response_data['message'].startswith("received")):
         logging.error(f"Failed to post image data for {shot_id} / {device_name}: {response_data}")
     
     else:
