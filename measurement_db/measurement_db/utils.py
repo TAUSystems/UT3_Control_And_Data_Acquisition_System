595f163a8959d3d410807b3708863ec33781ae97
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-03-06 17:56:19 -0800

    Get SQLAlchemy URL from MEASUREMENT_DB_SQLALCHEMY_URL first before URL.create
    I didn't manage to make an SQLite connection with driver and hostname.

--- measurement_db/measurement_db/utils.py
+++ measurement_db/measurement_db/utils.py
@@ -5,11 +5,15 @@ from dotenv import dotenv_values
 env = dotenv_values()
 
 def get_sqlalchemy_engine():
-    return create_engine(URL.create(env.get('MEASUREMENT_DB_SQLALCHEMY_DRIVER'),
-                            host=env.get('MEASUREMENT_DB_HOST'),
-                            port=env.get('MEASUREMENT_DB_PORT'),  
-                            database=env.get('MEASUREMENT_DB_DBNAME'),
-                            username=env.get('MEASUREMENT_DB_USERNAME'),
-                            password=env.get('MEASUREMENT_DB_PASSWORD'),
-                        ))
+    if sqlalchemy_url := env.get('MEASUREMENT_DB_SQLALCHEMY_URL'):
+        return create_engine(sqlalchemy_url)
+    
+    else:
+        return create_engine(URL.create(env.get('MEASUREMENT_DB_SQLALCHEMY_DRIVER'),
+                                host=env.get('MEASUREMENT_DB_HOST'),
+                                port=env.get('MEASUREMENT_DB_PORT'),  
+                                database=env.get('MEASUREMENT_DB_DBNAME'),
+                                username=env.get('MEASUREMENT_DB_USERNAME'),
+                                password=env.get('MEASUREMENT_DB_PASSWORD'),
+                            ))
 
