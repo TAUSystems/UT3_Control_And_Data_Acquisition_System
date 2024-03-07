2ba8f3564eccf21fc51d4bf5d0b6210d8061a998
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-03-06 16:43:32 -0800

    Allow unset environment variables in get_sqlalchemy_engine
    For those cases where not all parameters are required, such as
    with sqlite.

--- measurement_db/measurement_db/utils.py
+++ measurement_db/measurement_db/utils.py
@@ -5,11 +5,11 @@ from dotenv import dotenv_values
 env = dotenv_values()
 
 def get_sqlalchemy_engine():
-    return create_engine(URL.create(env['MEASUREMENT_DB_SQLALCHEMY_DRIVER'],
-                            host=env['MEASUREMENT_DB_HOST'],
-                            port=env['MEASUREMENT_DB_PORT'],  
-                            database=env['MEASUREMENT_DB_DBNAME'],
-                            username=env['MEASUREMENT_DB_USERNAME'],
-                            password=env['MEASUREMENT_DB_PASSWORD'],
+    return create_engine(URL.create(env.get('MEASUREMENT_DB_SQLALCHEMY_DRIVER'),
+                            host=env.get('MEASUREMENT_DB_HOST'),
+                            port=env.get('MEASUREMENT_DB_PORT'),  
+                            database=env.get('MEASUREMENT_DB_DBNAME'),
+                            username=env.get('MEASUREMENT_DB_USERNAME'),
+                            password=env.get('MEASUREMENT_DB_PASSWORD'),
                         ))
 
