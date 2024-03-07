0feab6bd21adffa27c5c7116ef3633b3e52094bd
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-03-07 15:40:08 -0800

    Measurement db schema updates: index, unique, relationship
    * Add unique constraints and index attributes to a few columns
    * Add ORM relationships (only referencing parents for now). This
      involves renaming the foreign key columns (e.g. scan to
      scan_timestamp)

--- measurement_db/measurement_db/orm/tables.py
+++ measurement_db/measurement_db/orm/tables.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from datetime import datetime
 
 from sqlalchemy import Double, ForeignKey, DateTime, String, Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 
@@ -14,8 +14,8 @@ class Base(DeclarativeBase):
 class Variable(Base):
     __tablename__ = "variable"
 
-    name: Mapped[str] = mapped_column(String(252), primary_key=True)
-    id: Mapped[int] = mapped_column(autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(252), index=True)
     description: Mapped[str] = mapped_column(String(252), default="")
     units: Mapped[Optional[str]] = mapped_column(String(16))
     display_units: Mapped[Optional[str]] = mapped_column(String(16))
@@ -31,7 +31,7 @@ class Session(Base):
     __tablename__ = "session"
 
     timestamp: Mapped[datetime] = mapped_column(DateTime(), primary_key=True, default=func.current_timestamp())
-    title: Mapped[str] = mapped_column(String(252), default="")
+    title: Mapped[str] = mapped_column(String(252), unique=True, index=True)
     description: Mapped[str] = mapped_column(Text(), default="")
 
     operator: Mapped[str] = mapped_column(String(252), default="", doc="Name of the person or organization operating this session.")
@@ -40,34 +40,42 @@ class Scan(Base):
     __tablename__ = "scan"
 
     timestamp: Mapped[datetime] = mapped_column(DateTime(), primary_key=True, default=func.current_timestamp())
-    session: Mapped[datetime] = mapped_column(ForeignKey('session.timestamp'))
-    number: Mapped[int] = mapped_column(index=True, doc="Sequence number of this scan within session")
+    session_timestamp: Mapped[datetime] = mapped_column(ForeignKey('session.timestamp'))
+    seq: Mapped[int] = mapped_column(index=True, doc="Sequence number of this scan within session")
 
     description: Mapped[str] = mapped_column(Text(), default="")
 
     notes: Mapped[str] = mapped_column(Text(), default="")
 
+    session: Mapped[Session] = relationship()
 
 class Burst(Base):
     __tablename__ = "burst"
 
     timestamp: Mapped[datetime] = mapped_column(DateTime(), primary_key=True)
-    scan: Mapped[datetime] = mapped_column(ForeignKey('scan.timestamp'))
+    scan_timestamp: Mapped[datetime] = mapped_column(ForeignKey('scan.timestamp'))
     seq: Mapped[int] = mapped_column(doc="Sequence number of this burst within scan")
 
     number_of_shots: Mapped[int]
     repetition_rate: Mapped[float] = mapped_column(Double, doc="Repetition rate in Hertz")
 
+    scan: Mapped[Scan] = relationship()
+
 class Shot(Base):
     __tablename__ = "shot"
 
     timestamp: Mapped[datetime] = mapped_column(DateTime(), primary_key=True)
-    burst: Mapped[datetime] = mapped_column(ForeignKey('burst.timestamp'))
+    burst_timestamp: Mapped[datetime] = mapped_column(ForeignKey('burst.timestamp'))
     seq: Mapped[int] = mapped_column(doc="Sequence number of this shot within burst")
+
+    burst: Mapped[Burst] = relationship()
 
 class Measurement(Base):
     __tablename__ = "measurement"
 
-    shot: Mapped[datetime] = mapped_column(ForeignKey('shot.timestamp'), primary_key=True)
+    shot_timestamp: Mapped[datetime] = mapped_column(ForeignKey('shot.timestamp'), primary_key=True)
     variable_id: Mapped[int] = mapped_column(ForeignKey('variable.id'), primary_key=True)
     value: Mapped[float] = mapped_column(Double)
+
+    shot: Mapped[Shot] = relationship()
+    variable: Mapped[Variable] = relationship()
