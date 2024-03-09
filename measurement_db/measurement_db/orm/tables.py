84550ae098a36071c2f05abfe2adbfe1f108bf56
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-03-08 16:55:52 -0800

    Update measurement db tables definition
    * Variable table: add source and epics_access_protocol columns, as Enums
    * Add back_populates to shot/burst bi-directional relationship

--- measurement_db/measurement_db/orm/tables.py
+++ measurement_db/measurement_db/orm/tables.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-import enum
-from typing import List, Optional
-from pathlib import Path
+from enum import Enum
+from typing import Optional
 from datetime import datetime
 
 from sqlalchemy import Double, ForeignKey, DateTime, String, Text
@@ -13,15 +12,45 @@ from sqlalchemy.sql import func
 class Base(DeclarativeBase):
     pass
 
+class VariableSource(Enum):
+    """ Describes how the variable values are obtained for the measurement table
+
+    manual: entered by some other process than this client or the image backend. 
+        should be uncommon.
+    monitor: monitor a PV and write to db on-change. This is good for when the value 
+        updates on every shot trigger (such as laser energy)
+    fetch: fetch a PV when a shot is detected (through a common trigger PV). This 
+        is good for when the value does not follow the shot trigger (such as motor 
+        position)
+    image_backend: 
+        variable is an output of analysis on the image backend
+    """
+    manual = 0
+    monitor = 1
+    fetch = 2
+    image_backend = 3
+
+class EPICSAccessProtocol(Enum):
+    """ Protocols by which a PV can be accessed
+    """
+    channel_access = 0
+    pvAccess = 1
+
 class Variable(Base):
     __tablename__ = "variable"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(252), index=True)
     description: Mapped[str] = mapped_column(String(252), default="")
+    
+    # units
     units: Mapped[Optional[str]] = mapped_column(String(16))
     display_units: Mapped[Optional[str]] = mapped_column(String(16))
     display_unit_multiplier: Mapped[Optional[float]] = mapped_column(Double)
+
+    # EPICS access and monitoring
+    source: Mapped[VariableSource]
+    epics_access_protocol: Mapped[Optional[EPICSAccessProtocol]]
 
 class ImageDevice(Base):
     __tablename__ = "image_device"
@@ -62,7 +91,7 @@ class Burst(Base):
     repetition_rate: Mapped[float] = mapped_column(Double, doc="Repetition rate in Hertz")
 
     scan: Mapped[Scan] = relationship()
-    shots: Mapped[list[Shot]] = relationship()
+    shots: Mapped[list[Shot]] = relationship(back_populates='burst')
 
 class Shot(Base):
     __tablename__ = "shot"
@@ -71,7 +100,7 @@ class Shot(Base):
     burst_timestamp: Mapped[datetime] = mapped_column(ForeignKey('burst.timestamp'))
     seq: Mapped[int] = mapped_column(doc="Sequence number of this shot within burst")
 
-    burst: Mapped[Burst] = relationship()
+    burst: Mapped[Burst] = relationship(back_populates='shots')
 
 class Measurement(Base):
     __tablename__ = "measurement"
