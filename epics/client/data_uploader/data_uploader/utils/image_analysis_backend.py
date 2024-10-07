b866b99562f38119875464f195e9884d8541db09
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-10-07 14:37:15 -0500

    Also remove time zone info from parse_shot_id result
    In ebec31f5ea22c13f55825ee568cb3052de2cf1b4, the UTC time zone was
    removed from Burst, Shot, Scan, Session objects. A helper function for
    the image backend must also return a tz-naive datetime. In at least one
    case, the result of this function was subtracted from a Shot.timestamp.

--- epics/client/data_uploader/data_uploader/utils/image_analysis_backend.py
+++ epics/client/data_uploader/data_uploader/utils/image_analysis_backend.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def parse_shot_id(shot_id: str) -> tuple[datetime, datetime]:
     burst_str, shot_str = shot_id.split('/')
-    burst_datetime = datetime.strptime(burst_str, "burst-%Y-%m-%dT%H-%M-%S-%f%z")  # the %z will pick up the Z as utc
-    shot_datetime = datetime.strptime(shot_str, "shot-%Y-%m-%dT%H-%M-%S-%f%z")
+    burst_datetime = datetime.strptime(burst_str, "burst-%Y-%m-%dT%H-%M-%S-%f%z").replace(tzinfo=None)  # the %z will pick up the Z as utc
+    shot_datetime = datetime.strptime(shot_str, "shot-%Y-%m-%dT%H-%M-%S-%f%z").replace(tzinfo=None)
     return burst_datetime, shot_datetime
 
