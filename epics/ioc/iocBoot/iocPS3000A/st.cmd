5662e44ef20aee1288a7e89f56b1f75324df0d16
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2025-03-04 13:57:05 -0800

    Add ICT records to picoApp
    - db with records
    - load records in st.cmd
    - include ict.db in Makefile
    - additional calc dependency in src/Makefile and *Include.dbd
    - add paths to dependencies in RELEASE

--- epics/ioc/iocBoot/iocPS3000A/st.cmd
+++ epics/ioc/iocBoot/iocPS3000A/st.cmd
@@ -8,9 +8,18 @@ PS3000A_registerRecordDeviceDriver(pdbbase)
 
 PS3000AConfigure("ps3000a_port", 1000)
 
-dbLoadRecords("../../db/PS3000A.db","P=TEST:,R=scope1:,PORT=ps3000a_port,ADDR=0,TIMEOUT=1,NPOINTS=1000")
-dbLoadRecords("../../db/PS3000A_ch.db","P=TEST:,R=scope1:,PORT=ps3000a_port,ADDR=0,TIMEOUT=1,NPOINTS=1000")
-dbLoadRecords("../../db/asynRecord.db","P=TEST:,R=asyn1,PORT=ps3000a_port,ADDR=0,OMAX=80,IMAX=80")
-#asynSetTraceMask("ps3000a_port",0,0xff)
-asynSetTraceIOMask("ps3000a_port",0,0x2)
+epicsEnvSet(P, "Electrons:")
+epicsEnvSet(R, "ICT:")
+epicsEnvSet(PORT, "ps3000a_port")
+
+dbLoadRecords("../../db/PS3000A.db","P=$(P),R=$(R),PORT=$(PORT),ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("../../db/PS3000A_ch.db","P=$(P),R=$(R),PORT=$(PORT),ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("../../db/ict.db","P=$(P),R=$(R),C=B,NPOINTS=1000")
+
+dbLoadRecords("../../db/asynRecord.db","P=Electrons:,R=ps3000a_asyn,PORT=$(PORT),ADDR=0,OMAX=80,IMAX=80")
+
+#asynSetTraceMask("$(PORT)",0,0xff)
+asynSetTraceIOMask("$(PORT)",0,0x2)
 iocInit()
+
+dbpf $(P)$(R)Sensitivity 0.5
