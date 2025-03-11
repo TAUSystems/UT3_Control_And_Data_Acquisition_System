48478e6abc735257882d1417915809af6416c66c
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2025-03-11 09:25:16 -0700

    Update ICT records to reflect PS changes and misunderstandings
    * ICT waveform is now on channel A
    * Don't convert counts to volts - ICT waveform is already in microVolt
    * Use SampleLength_RBV - TimePerDiv is an oscilloscope division, not
      sampling interval
    
    82df1bad removed a lot of unnecessary and incorrect items from the driver

--- epics/ioc/iocBoot/iocPS3000A/st.cmd
+++ epics/ioc/iocBoot/iocPS3000A/st.cmd
@@ -14,7 +14,8 @@ epicsEnvSet(PORT, "ps3000a_port")
 
 dbLoadRecords("../../db/PS3000A.db","P=$(P),R=$(R),PORT=$(PORT),ADDR=0,TIMEOUT=1,NPOINTS=1000")
 dbLoadRecords("../../db/PS3000A_ch.db","P=$(P),R=$(R),PORT=$(PORT),ADDR=0,TIMEOUT=1,NPOINTS=1000")
-dbLoadRecords("../../db/ict.db","P=$(P),R=$(R),C=B,NPOINTS=1000")
+
+dbLoadRecords("../../db/ict.db","P=$(P),R=$(R),C=A,NPOINTS=1000")
 
 dbLoadRecords("../../db/asynRecord.db","P=$(P),R=ps3000a_asyn,PORT=$(PORT),ADDR=0,OMAX=80,IMAX=80")
 
