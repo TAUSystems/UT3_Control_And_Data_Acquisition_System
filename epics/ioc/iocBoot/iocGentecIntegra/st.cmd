7b35cfd999c1dd63e50b9ee8629083af66fe04ba
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2025-04-16 16:00:06 -0700

    Update iocGentecIntegra/st.cmd to connect our energy meter

--- epics/ioc/iocBoot/iocGentecIntegra/st.cmd
+++ epics/ioc/iocBoot/iocGentecIntegra/st.cmd
@@ -5,16 +5,24 @@
 
 < envPaths
 
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/GentecIntegra.dbd"
+dbLoadDatabase "../../dbd/GentecIntegra.dbd"
 GentecIntegra_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/GentecIntegra.db","user=reinier")
+# Configure Energy Meter port
+epicsEnvSet ("STREAM_PROTOCOL_PATH", "../../db")
 
-cd "${TOP}/iocBoot/${IOC}"
+drvAsynSerialPortConfigure ("EnergyMeter1","/dev/ttyS1")
+asynSetOption ("EnergyMeter1", 0, "baud", "9600")
+asynSetOption ("EnergyMeter1", 0, "bits", "8")
+asynSetOption ("EnergyMeter1", 0, "parity", "none")
+asynSetOption ("EnergyMeter1", 0, "stop", "1")
+asynSetOption ("EnergyMeter1", 0, "clocal", "Y")
+asynSetOption ("EnergyMeter1", 0, "crtscts", "N")
+
+## Load record instances
+dbLoadRecords("../../db/GentecIntegra.db","P=Laser,R=Energy,PORT=EnergyMeter1")
+
 iocInit
 
 ## Start any sequence programs
