d32aef85a881b0383def07976fd7d35be2013eaf
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2025-08-27 20:14:27 +0200

    Update horizontal.zaber startup script for pinholes
    TODO:
    * find out correct USB ports
    * check motor settings

--- epics/ioc/PinholeMotionTop/iocBoot/iocHorizontalZaber/horizontal.zaber.cmd
+++ epics/ioc/PinholeMotionTop/iocBoot/iocHorizontalZaber/horizontal.zaber.cmd
@@ -16,8 +16,6 @@ dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=zaberMotion:")
 ###
 ZaberMotionSetDbPath("/home/ubuntu/Downloads/devices-public.sqlite")
 
-dbLoadTemplate("motor.substitutions.zaber")
-
 ###
 # ZaberMotionCreateController(
 #    asyn motor port (will be created),
@@ -28,12 +26,12 @@ dbLoadTemplate("motor.substitutions.zaber")
 #    zaber device number (1-indexed)
 # )
 ###
-ZaberMotionCreateController("XMCC2", 2, 100, 5000, "serial:///dev/ttyUSB0", 1)
-ZaberMotionCreateController("XLDQ",  1, 100, 5000, "serial:///dev/ttyUSB0", 2)
-ZaberMotionCreateController("XFWRE", 1, 100, 5000, "serial:///dev/ttyUSB0", 3)
-ZaberMotionCreateController("XMCC2_tcp", 2, 100, 5000, "tcp://my.local.controller", 1)
+ZaberMotionCreateController("DivergencePinholePort",  1, 100, 5000, "serial:///dev/ttyUSB0", 0)
+ZaberMotionCreateController("EnergyPinholePort",      1, 100, 5000, "serial:///dev/ttyUSB1", 0)
 
 cd "${TOP}/iocBoot/${IOC}"
+dbLoadTemplate("horizontal.zaber.substitutions")
+
 iocInit
 
 ## motorUtil (allstop & alldone)
